5a9e439a00fff9ac111490d74ad35e6b6b971b5b
AkashAich1999 <108985323+AkashAich1999@users.noreply.github.com>
2024-04-24 21:38:46 +0530

    Frontend Routing Implemented

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,9 +1,28 @@
 import './App.css';
+import { RouterProvider, createBrowserRouter,  } from "react-router-dom";
+import Signup from './components/Signup';
+import HomePage from './components/HomePage';
+import Login from './components/Login';
+
+const router = createBrowserRouter([
+   {
+    path:"/",
+    element:<HomePage/>
+   },
+   {
+    path:"/register",
+    element:<Signup/>
+   },
+   {
+    path:"/login",
+    element:<Login />
+   },
+]);
 
 function App() {
   return (
     <div className="App">
-      Let's Build Chat App
+      <RouterProvider router={router} />
     </div>
   );
 }
