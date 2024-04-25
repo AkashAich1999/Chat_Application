5320e99e8c115a3623589ef410127e76e73698db
AkashAich1999 <108985323+AkashAich1999@users.noreply.github.com>
2024-04-25 10:10:54 +0530

    Sign Up Page Coding Started

--- frontend/src/components/Signup.jsx
+++ frontend/src/components/Signup.jsx
@@ -2,7 +2,53 @@ import React from 'react'
 
 const Signup = () => {
   return (
-    <div>Signup</div>
+    <div className="min-w-96 mx-auto">
+      <div className="w-full p-6 rounded-lg shadow-md bg-gray-400 bg-clip-padding backdrop-filter backdrop-blur-md bg-opacity-10 border border-gray-100">
+        <h1 className="text-3xl font-bold text-center">Sign Up</h1>
+        <form action="">
+          <div>
+            <label className="label p-2">
+              <span className="text-base label-text">Full Name</span>
+            </label>
+            <input 
+              className="w-full input input-bordered h-10"
+              type="text"
+              placeholder="Fullname"  
+            />
+          </div>
+          <div>
+            <label className="label p-2">
+              <span className="text-base label-text">User Name</span>
+            </label>
+            <input 
+              className="w-full input input-bordered h-10"
+              type="text"
+              placeholder="Username"  
+            />
+          </div>
+          <div>
+            <label className="label p-2">
+              <span className="text-base label-text">Password</span>
+            </label>
+            <input 
+              className="w-full input input-bordered h-10"
+              type="password"
+              placeholder="Password"  
+            />
+          </div>
+          <div>
+            <label className="label p-2">
+              <span className="text-base label-text">Confirm Password</span>
+            </label>
+            <input 
+              className="w-full input input-bordered h-10"
+              type="password"
+              placeholder="Password"  
+            />
+          </div>
+        </form>
+      </div>
+    </div>
   )
 }
 
